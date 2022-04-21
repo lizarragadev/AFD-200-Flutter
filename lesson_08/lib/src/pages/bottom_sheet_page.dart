05fd54d3a59709c0bcc913004cf4238a6139cc00
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-04-20 22:21:48 -0400

    final code lesson 8

--- lesson_08/lib/src/pages/bottom_sheet_page.dart
+++ lesson_08/lib/src/pages/bottom_sheet_page.dart
@@ -1,20 +1,105 @@
 import 'package:flutter/material.dart';
 
-class BottomSheetPage extends StatefulWidget {
-  const BottomSheetPage({Key? key}) : super(key: key);
+class BottomSheetPage extends StatelessWidget {
+  BottomSheetPage({Key? key}) : super(key: key);
 
-  @override
-  State<BottomSheetPage> createState() => _BottomSheetPageState();
-}
+  final _key = GlobalKey<ScaffoldState>();
 
-class _BottomSheetPageState extends State<BottomSheetPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        key: _key,
         appBar: AppBar(
           title: Text('Bottom Sheets'),
         ),
-        body: Text("Hola")
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 20,),
+              ElevatedButton(
+                child: Text("Abrir Bottom Modal Sheet"),
+                onPressed: () { _showModalBottomSheet(context); },
+              ),
+              SizedBox(height: 20,),
+              ElevatedButton(
+                child: Text("Abrir Persistent Bottom Sheet"),
+                onPressed: () { _showPersistentBottomSheet(context); },
+              ),
+              SizedBox(height: 20,),
+              ElevatedButton(
+                child: Text("Probar boton"),
+                onPressed: () { print("Funciona boton"); },
+              )
+            ],
+          ),
+        )
     );
   }
+
+  void _showModalBottomSheet(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+            height: 400.0,
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Text("Titulo del Bottom Sheet", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+                  SizedBox(height: 20,),
+                  Image(image: AssetImage("images/logo_flutter.png"), width: 150.0,),
+                  SizedBox(height: 20,),
+                  ElevatedButton(
+                      onPressed: () { Navigator.of(context).pop(); },
+                      child: Text("Cerrar bottom Sheet")
+                  )
+                ],
+              ),
+            ),
+          );
+        }
+    );
+  }
+
+  void _showPersistentBottomSheet(context) {
+    _key.currentState?.showBottomSheet((context) {
+      return Container(
+        height: 400.0,
+        color: Colors.amberAccent,
+        alignment: Alignment.topCenter,
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Text("Titulo del Bottom Sheet", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+              SizedBox(height: 20,),
+              Image(image: AssetImage("images/logo_flutter.png"), width: 150.0,),
+              SizedBox(height: 20,),
+              ElevatedButton(
+                  onPressed: () { Navigator.of(context).pop(); },
+                  child: Text("Cerrar bottom Sheet")
+              )
+            ],
+          ),
+        ),
+      );
+    });
+  }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
