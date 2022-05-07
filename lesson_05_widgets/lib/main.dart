120c65fb1868fd8f632259be63f8f70e179225fc
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-05-06 22:43:44 -0400

    delete lesson 6

--- lesson_05_widgets/lib/main.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:lesson_05_widgets/src/my_app.dart';
-
-void main() => runApp(MyApp());
--- lesson_06_navigation/lib/main.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:lesson_06_navigation/src/my_app.dart';
-
-void main() => runApp(const MyApp());
